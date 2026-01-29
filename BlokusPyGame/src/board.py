73686bc0566631add0878dddd4769c83d3f1c4eb
Brayden O'Neal <71409691+braydenoneal@users.noreply.github.com>
2026-01-29 13:35:30 -0600

    Fix issues with not using the Piece class correctly

--- BlokusPyGame/src/board.py
+++ BlokusPyGame/src/board.py
@@ -15,76 +15,73 @@ class Board:
             return [(0, 0), (self.size-1, self.size-1)]
 
     def _within_bounds(self, piece) -> bool:
-        for pos in piece:
+        for pos in piece.shape_val:
             if pos[0] < 0 or pos[0] >= self.size or pos[1] < 0 or pos[1] >= self.size:
                 return False
 
         return True
 
-    def can_place_starting_piece(self, piece, player):
+    def can_place_starting_piece(self, piece):
         # piece: [(0,0), (0,1)...] already transformed to its position
-        # player: int 1-4
         if not self._within_bounds(piece):
             return False
 
-        corner = self.starting_corners[player - 1]
+        corner = self.starting_corners[piece.color - 1]
         touches_corner = False
 
-        for pos in piece:
+        for pos in piece.shape_val:
             if pos == corner:
                 touches_corner = True
                 break
 
         return touches_corner
 
-    def can_place_piece(self, piece, player):
+    def can_place_piece(self, piece):
         # piece: [(0,0), (0,1)...] already transformed to its position
-        # player: int 1-4
         if not self._within_bounds(piece):
             return False
 
         touches_player_corner = False
 
-        for pos in piece:
+        for pos in piece.shape_val:
             # Check for overlap
             if self.grid[pos[0]][pos[1]] != 0:
                 return False
 
             # Check for edge-to-edge
-            if pos[0] > 0 and self.grid[pos[0] - 1][pos[1]] == player:
+            if pos[0] > 0 and self.grid[pos[0] - 1][pos[1]] == piece.color:
                 return False
 
-            if pos[0] < self.size-1 and self.grid[pos[0] + 1][pos[1]] == player:
+            if pos[0] < self.size-1 and self.grid[pos[0] + 1][pos[1]] == piece.color:
                 return False
 
-            if pos[1] > 0 and self.grid[pos[0]][pos[1] - 1] == player:
+            if pos[1] > 0 and self.grid[pos[0]][pos[1] - 1] == piece.color:
                 return False
 
-            if pos[1] < self.size-1 and self.grid[pos[0]][pos[1] + 1] == player:
+            if pos[1] < self.size-1 and self.grid[pos[0]][pos[1] + 1] == piece.color:
                 return False
 
             # Check for corner-to-corner
             if touches_player_corner:
                 pass
-            elif pos[0] > 0 and pos[1] > 0 and self.grid[pos[0] - 1][pos[1] - 1] == player:
+            elif pos[0] > 0 and pos[1] > 0 and self.grid[pos[0] - 1][pos[1] - 1] == piece.color:
                 touches_player_corner = True
 
-            elif pos[0] > 0 and pos[1] < self.size-1 and self.grid[pos[0] - 1][pos[1] + 1] == player:
+            elif pos[0] > 0 and pos[1] < self.size-1 and self.grid[pos[0] - 1][pos[1] + 1] == piece.color:
                 touches_player_corner = True
 
-            elif pos[0] < self.size-1 and pos[1] > 0 and self.grid[pos[0] + 1][pos[1] - 1] == player:
+            elif pos[0] < self.size-1 and pos[1] > 0 and self.grid[pos[0] + 1][pos[1] - 1] == piece.color:
                 touches_player_corner = True
 
-            elif pos[0] < self.size-1 and pos[1] < self.size-1 and self.grid[pos[0] + 1][pos[1] + 1] == player:
+            elif pos[0] < self.size-1 and pos[1] < self.size-1 and self.grid[pos[0] + 1][pos[1] + 1] == piece.color:
                 touches_player_corner = True
 
         return touches_player_corner
 
-    def place_piece(self, piece, player):
+    def place_piece(self, piece):
         # piece: [(0,0), (0,1)...] already transformed to its position
-        # player: int 1-4
-        for pos in piece:
-            self.grid[pos[0]][pos[1]] = player
+        for pos in piece.shape_val:
+            self.grid[pos[0]][pos[1]] = piece.color
 
 if __name__ == "__main__":
     board = Board()
