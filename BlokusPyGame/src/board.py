2907fff42a8f775b46d03d9a7d2d360141670e65
Brayden O'Neal <71409691+braydenoneal@users.noreply.github.com>
2026-01-30 11:04:59 -0600

    Consolidate within bounds check into placement validation method

--- BlokusPyGame/src/board.py
+++ BlokusPyGame/src/board.py
@@ -8,28 +8,20 @@ class Board:
         self.starting_corners = self._get_starting_corners()
 
     def _get_starting_corners(self):
-        # For 4 players: all 4 corners
-        # For 2 players: opposite corners
         if self.num_players == 4:
             return [(0, 0), (0, self.size-1), 
                     (self.size-1, 0), (self.size-1, self.size-1)]
         else:  # 2 players
             return [(0, 0), (self.size-1, self.size-1)]
 
-    def _within_bounds(self, piece: Piece) -> bool:
-        for pos in piece.cells():
-            if pos[0] < 0 or pos[0] >= self.size or pos[1] < 0 or pos[1] >= self.size:
-                return False
-
-        return True
-
     def can_place_piece(self, piece: Piece):
-        if not self._within_bounds(piece):
-            return False
-
         touches_player_corner = False
 
         for pos in piece.cells():
+            # Check for bounds
+            if pos[0] < 0 or pos[0] >= self.size or pos[1] < 0 or pos[1] >= self.size:
+                return False
+
             # Check for overlap
             if self.grid[pos[0]][pos[1]] != 0:
                 return False
